3ff1be773d520bc4d3417e7d84aa1b8fb27e2f54
kiranjugdar <kiranjugdar@gmail.com>
2023-08-03 13:57:18 -0400

    Code change

--- src/main/java/com/api/LambdaJavaAPI.java
+++ src/main/java/com/api/LambdaJavaAPI.java
@@ -10,7 +10,7 @@ public class LambdaJavaAPI implements RequestHandler<Object, GatewayResponse> {
     @Override
     public GatewayResponse handleRequest(Object object, Context context) {
 
-        String message = "Hello 3";
+        String message = "Hello 4";
         System.out.println(message);
 
         GatewayResponse response = new GatewayResponse(
